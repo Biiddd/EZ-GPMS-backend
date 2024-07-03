753b0ef673d7ef50d204cab0bfe6c2fd22369683
Biiddd <1256773838@qq.com>
2024-07-03 22:33:03 +0800

    perf(uploadAsk): 删除多余文件

--- router_handler/uploadAsk_handler.js
+++ router_handler/uploadAsk_handler.js
@@ -1,13 +1,6 @@
 const db = require('../db/index');
 const logger = require('../modules/logger');
 
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log('MySQL Connected...');
-});
-
 exports.uploadAsk = (req, res) => {
   const { task, instruct } = req.body;
   const userId = '222222222222'; // 用户id默认为222222222222
