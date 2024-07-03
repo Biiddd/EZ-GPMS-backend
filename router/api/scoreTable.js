bba81970cf18eda6aaad74173fdf2e4434fd1277
xuan <1438362554@qq.com>
2024-07-03 20:17:13 +0800

    fix(scoreTable.js): 删除重复代码

--- router/api/scoreTable.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const scoreTableHandler = require("../../router_handler/scoreTable_handler");
-
-router.get("/api/scoreTable", scoreTableHandler.scoreTable);
-
-module.exports = router;
