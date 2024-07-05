7564cbd035092de4db2485eff7a447cb35aa0038
malibin <malibin02@163.com>
2024-07-05 14:16:10 +0800

    refactor(Login): 规范接口返回业务状态码

--- router_handler/getLogin_handler.js
+++ router_handler/getLogin_handler.js
@@ -14,13 +14,13 @@ const isCorrect = (req, res) => {
     db.query(query, [user_id], async (err, rows) => {
         if (err) {
             logger.error("数据库查询失败");
-            return res.status(500).json({ status: "未知错误请重试" });
+            return res.status(200).json({code:20005, message: "未知错误请重试" ,data:{type:''}});
         }
 
         if (rows.length === 0) {
             // 用户名不存在
             logger.warn("用户名不存在");
-            return res.status(401).json({ status: "用户名不存在" });
+            return res.status(200).json({ code:20004, message: "用户名不存在",data:{type:''}});
         }
 
         // 获取数据库中的密码哈希值
@@ -58,17 +58,17 @@ const isCorrect = (req, res) => {
 
                     // 返回带有教师类型的信息
                     logger.info("教师登录成功");
-                    return res.json({ status: "teacher success", teacher_type: teacherRows[0].teacher_type });
+                    return res.json({ code:20002, message: "success",data:{type:teacherRows[0].teacher_type} });
                 });
             } else {
                 // 普通用户登录成功
                 logger.info("登录成功");
-                return res.json({ status: "success" });
+                return res.json({ code:20001, message: "success",data:{type:rows[0].type} });
             }
         } else {
             // 密码不匹配
             logger.warn("密码不匹配");
-            return res.status(401).json({ status:  "用户名或密码错误" });
+            return res.status(200).json({ code:20003, message: "用户名或密码错误",data:{type:''}});
         }
     });
 };
