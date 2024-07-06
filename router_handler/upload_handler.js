99044ced06e43db570910e9957b8239d4f255925
Biiddd <1256773838@qq.com>
2024-07-07 01:53:56 +0800

    fix(upload): 补全上传文件类别判断

--- router_handler/upload_handler.js
+++ router_handler/upload_handler.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         const {stu_id} = req.body;
 
-        // 设置文件路径 uploads/file_id
+        // 设置文件路径 uploads/stu_id
         const uploadDir = path.resolve(__dirname, `../uploads/${stu_id}`);
         if (!fs.existsSync(uploadDir)) {
             fs.mkdirSync(uploadDir, {recursive: true});
@@ -17,7 +17,7 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         const {filename} = req.body;
-        // 用 file_id filename 重命名文件
+        // 用 stu_id filename 重命名文件
         const uniqueFilename = `${filename}${path.extname(file.originalname)}`;
         cb(null, uniqueFilename);
     }
@@ -41,8 +41,7 @@ exports.uploadSingle = (req, res) => {
         }
         console.log(req.body)
         const {stu_id, filename} = req.body;
-        const filePath = path.join(`uploads/${stu_id}`, file.filename);
-
+        const filePath = path.join(`uploads/${stu_id}`, file.filename).replace(/\\/g, '/');
 
         // 构建 SQL 更新语句
         let query;
@@ -50,36 +49,42 @@ exports.uploadSingle = (req, res) => {
         switch (filename) {
             case '开题报告':
                 query = `UPDATE file
-                         SET file_start = ?
-                         WHERE file_id = ${stu_id}`;
+                         SET start_file = ?
+                         WHERE stu_id = ${stu_id}`;
                 break;
             case '外文翻译':
                 query = `UPDATE file
-                         SET file_translation = ?
-                         WHERE file_id = ${stu_id}`;
+                         SET translation_file = ?
+                         WHERE stu_id = ${stu_id}`;
                 break;
             case '校外毕设申请':
                 query = `UPDATE file
-                         SET file_outschool = ?
-                         WHERE file_id = ${stu_id}`;
+                         SET outschool_file = ?
+                         WHERE stu_id = ${stu_id}`;
                 break;
             case '中期成果':
                 query = `UPDATE file
-                         SET file_mid = ?
-                         WHERE file_id = ${stu_id}`;
+                         SET midWork_file = ?
+                         WHERE stu_id = ${stu_id}`;
                 break;
-            case '终稿':
+
+            case '中期报告':
                 query = `UPDATE file
-                         SET result_mid = ?
-                         WHERE file_id = ${stu_id}`;
+                         SET midReport_file = ?
+                         WHERE stu_id = ${stu_id}`;
+                break;
+            case '毕业论文终稿':
+                query = `UPDATE file
+                         SET final_file = ?
+                         WHERE stu_id = ${stu_id}`;
                 break;
             case '答辩申请表':
                 query = `UPDATE file
-                         SET file_defense = ?
-                         WHERE file_id = ${stu_id}`;
+                         SET defenseApply_file = ?
+                         WHERE stu_id = ${stu_id}`;
                 break;
-            default:
-                return res.status(400).send('未知的文件类型');
+            // default:
+            //     return res.status(400).send('未知的文件类型');
         }
 
         // 执行数据库更新操作
