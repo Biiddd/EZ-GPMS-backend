02044b6ca8e18c3f894d7c9f52d90144976bf3d9
Biiddd <1256773838@qq.com>
2024-07-08 21:00:52 +0800

    perf(getGroup): 优化获取分组信息查询接口

--- router/api/getGroup.js
+++ router/api/getGroup.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const getGroupHandler = require('../../router_handler/getGroup_handler');
 
-router.post('/api/stu/getGroup', getGroupHandler.getScore);
+router.get('/api/admin/getGroup', getGroupHandler.get);
 
 module.exports = router;
--- router_handler/getGroup_handler.js
+++ router_handler/getGroup_handler.js
@@ -1,22 +1,29 @@
 const db = require('../db/index');
 const logger = require('../modules/logger');
 
-exports.getScore = (req, res) => {
-  let { group_id } = req.body;
+exports.get = (req, res) => {
+  // let { group_id } = req.body;
   const query = `
-    SELECT *
-    FROM stu_group
-    JOIN teach_group ON stu_group.group_id = teach_group.group_id
-    WHERE stu_group.group_id = ?`;
+      SELECT g.group_id,
+             GROUP_CONCAT(DISTINCT CONCAT(u1.name) ORDER BY stu.stu_id SEPARATOR ',')         AS stu_list,
+             GROUP_CONCAT(DISTINCT CONCAT(u2.name) ORDER BY teacher.teacher_id SEPARATOR ',') AS teacher_list
+      FROM \`group\` g
+               LEFT JOIN
+           stu ON stu.stu_group_id = g.stu_group_id
+               LEFT JOIN
+           \`user\` u1 ON stu.stu_id = u1.user_id
+               LEFT JOIN
+           teacher ON teacher.teacher_group_id = g.teacher_group_id
+               LEFT JOIN
+           \`user\` u2 ON teacher.teacher_id = u2.user_id
+      GROUP BY g.group_id;`;
 
-  db.query(query, [group_id], (err, rows) => {
+  db.query(query, [], (err, rows) => {
     if (err) {
       logger.error('查询数据库时出错:', err);
-      return res.status(500).send('查询数据库时出错');
+      return res.status(200).json({ code: 200500, msg: '查询数据库时出错' });
     }
     logger.info('获取分组数据成功');
-    const group = rows[0];
-
-    res.status(200).json(group);
+    res.status(200).json({ code: 200, msg: '获取分组数据成功', data: rows });
   });
 };
