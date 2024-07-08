11320986ebcc8e0969b73dc219e6494c72885619
Biiddd <1256773838@qq.com>
2024-07-08 14:16:31 +0800

    feat(upload): 添加教师打分流程的当前状态和权限接口

--- router/api/getState.js
+++ router/api/getState.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const getStateHandler = require('../../router_handler/getState_handler');
 
 router.post('/api/stu/getState', getStateHandler.getInfo);
+router.post('/api/teacher/getState', getStateHandler.getTeacherState);
 
 module.exports = router;
